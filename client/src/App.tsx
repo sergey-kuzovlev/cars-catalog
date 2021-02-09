a6aada893ccc83a31b1ef1fb89f8085e7319ea05
sergey_kuzovlev_dev <sergey.kuzovlev.dev@gmail.com>
2021-02-09 18:23:16 +0200

    add main image with text; fix minor issues

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,11 @@ function App() {
   return (
   <div className="App">
     <Navbar />
+    <div className="main-page-img">
+      <div className="text container">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </div>
+    </div>
     <Router history={history}>
       <Switch>
         <Route exact path="/" component={FilteredCars} />
--- client/src/containers/SortLink.ts
+++ client/src/containers/SortLink.ts
@@ -1,15 +1,5 @@
 import { connect } from 'react-redux'
-import { setVisibilityFilter } from '../actions'
 import { Filters } from '../components/carsTable/Filters'
-import { ICar } from '../interfaces/car.interface'
-
-// const mapStateToProps = (state: { visibilityFilter: string}, ownProps: any) => ({
-//   active: ownProps.filter === state.visibilityFilter
-// })
-
-// const mapDispatchToProps = (dispatch: any, ownProps: any) => ({
-//   makes: () => dispatch(setVisibilityFilter(ownProps.filter))
-// })
 
 const mapStateToProps = (state: {cars: any, visibilityFilter: string}): { makes: string[]} => ({
   makes: state.cars.makes
